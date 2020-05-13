9daae81ba70c6799213708480f5f0710300bc74c
Ryan Moragas <ryanmoragas@gmail.com>
2020-05-13 17:07:05 -0500

    hardcoded repo

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,5 +1,16 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo,
-    otp_app: :rumbl,
-    adapter: Ecto.Adapters.Postgres
+  # use Ecto.Repo,
+  #   otp_app: :rumbl,
+  #   adapter: Ecto.Adapters.Postgres
+
+  @moduledoc """
+    In memory repository.
+  """
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: "1", name: "José", username: "josevalim", password: "elixir"},
+    %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
+    %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}]
+  end
+
+  def all(_module), do: []
 end
