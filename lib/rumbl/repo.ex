3fba2bd08bc54410bef8f98f8d9e3246b940e4c0
Ryan Moragas <ryanmoragas@gmail.com>
2020-05-13 17:15:39 -0500

    finctions to fetch users from db

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -13,4 +13,14 @@ defmodule Rumbl.Repo do
   end
 
   def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
 end
