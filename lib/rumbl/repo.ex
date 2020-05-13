16464457278666b7f35e6489b4068131ac6099e4
Ryan Moragas <ryanmoragas@gmail.com>
2020-05-13 17:08:33 -0500

    fixed struct names

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -6,10 +6,10 @@ defmodule Rumbl.Repo do
   @moduledoc """
     In memory repository.
   """
-  def all(Rumbl.User) do
-    [%Rumbl.User{id: "1", name: "José", username: "josevalim", password: "elixir"},
-    %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
-    %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}]
+  def all(RumblWeb.User) do
+    [%RumblWeb.User{id: "1", name: "José", username: "josevalim", password: "elixir"},
+    %RumblWeb.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
+    %RumblWeb.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}]
   end
 
   def all(_module), do: []
