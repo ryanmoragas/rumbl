151caa19c6782bfc907289bbf0fbf2355a43dd39
Ryan Moragas <ryanmoragas@gmail.com>
2020-05-14 09:22:57 -0500

    fixed typo

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -6,4 +6,9 @@ defmodule RumblWeb.UserController do
     users = Repo.all(RumblWeb.User)
     render conn, "index.html", users: users
   end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(RumblWeb.User, id)
+    render conn, "show.html", user: user
+  end
 end
