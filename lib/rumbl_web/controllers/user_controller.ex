bb69b56e9b83a2f3116fe2682afb530c2f9f09f8
Ryan Moragas <ryanmoragas@gmail.com>
2020-05-18 14:43:16 -0500

    updated router, functional through pg 75

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -1,6 +1,7 @@
 defmodule RumblWeb.UserController do
   use RumblWeb, :controller
   alias Rumbl.Repo
+  alias RumblWeb.User
 
   def index(conn, _params) do
     users = Repo.all(RumblWeb.User)
@@ -10,5 +11,10 @@ defmodule RumblWeb.UserController do
   def show(conn, %{"id" => id}) do
     user = Repo.get(RumblWeb.User, id)
     render conn, "show.html", user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
   end
 end
