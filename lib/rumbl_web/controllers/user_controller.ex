feab063d1ab24a3cffc7d495db941ab340ad3c49
Ryan Moragas <ryanmoragas@gmail.com>
2020-05-19 11:07:56 -0500

    login functional, pg 95

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -25,6 +25,7 @@ defmodule RumblWeb.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> RumblWeb.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: Routes.user_path(conn, :index))
     {:error, changeset} ->
