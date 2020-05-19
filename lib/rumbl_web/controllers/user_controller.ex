e21891ac6562008f15c51cb4adb9301d3efe791f
Ryan Moragas <ryanmoragas@gmail.com>
2020-05-18 22:07:54 -0500

    authenitcation fully funcitonal, pg 94

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -1,10 +1,12 @@
 defmodule RumblWeb.UserController do
   use RumblWeb, :controller
+  plug :authenticate when action in [:index, :show]
+
   alias Rumbl.Repo
   alias RumblWeb.User
 
   def index(conn, _params) do
-    users = Repo.all(RumblWeb.User)
+    users = Repo.all(RumblWeb. User)
     render conn, "index.html", users: users
   end
 
@@ -27,6 +29,17 @@ defmodule RumblWeb.UserController do
         |> redirect(to: Routes.user_path(conn, :index))
     {:error, changeset} ->
       render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
     end
   end
 end
